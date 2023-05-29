b09dfd1da40b082077ed3ea6439cbf768b66672a
marcelowkr2 <marcelowkr@hotmail.com>
2023-05-29 20:28:15 -0300

    update_bd

--- BACKEND/src/infrastructure/database/index.js
+++ BACKEND/src/infrastructure/database/index.js
@@ -1,58 +1,37 @@
+const Sequelize = require("sequelize");
 const mysql = require('mysql');
-const fs = require('fs');
 
-var config =
-{
-    host: 'xp48.mysql.database.azure.com',
-    user: 'Marcelop',
-    password: 'dU2q4Lpm12',
-    database: 'xp48',
+
+
+const DB_NAME = "loja";
+const DB_USER = "Marcelop";
+const DB_PASS = "dU2q4Lpm12";
+const DB_CONFIG = {
+    dialect: "mysql",
+    host: "xp48.mysql.database.azure.com",
     port: 3306,
-    ssl: {ca: fs.readFileSync("your_path_to_ca_cert_file_DigiCertGlobalRootCA.crt.pem")}
 };
 
-const conn = new mysql.createConnection(config);
+// objeto para guardar a conexão do banco dados
+let db = {};
 
-conn.connect(
-    function (err) { 
-    if (err) { 
-        console.log("!!! Cannot connect !!! Error:");
-        throw err;
-    }
-    else
-    {
-       console.log("Connection established.");
-           queryDatabase();
-    }
+try {
+  db = new Sequelize(DB_NAME, DB_USER, DB_PASS, DB_CONFIG);
+} catch (error) {
+  console.error("Erro ao tentar uma conexão com banco dados");
+}
+
+async function hasConnection() {
+  try {
+    await db.authenticate();
+    console.log("Banco dados conectado!");
+  } catch (error) {
+    console.error("Erro ao tentar se conectar ao banco de dados");
+  }
+}
+
+Object.assign(db, {
+  hasConnection,
 });
 
-function queryDatabase(){
-    conn.query('DROP TABLE IF EXISTS inventory;', function (err, results, fields) { 
-        if (err) throw err; 
-        console.log('Dropped inventory table if existed.');
-    })
-        conn.query('CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);', 
-            function (err, results, fields) {
-                if (err) throw err;
-        console.log('Created inventory table.');
-    })
-    conn.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['banana', 150], 
-            function (err, results, fields) {
-                if (err) throw err;
-        else console.log('Inserted ' + results.affectedRows + ' row(s).');
-        })
-    conn.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['orange', 154], 
-            function (err, results, fields) {
-                if (err) throw err;
-        console.log('Inserted ' + results.affectedRows + ' row(s).');
-        })
-    conn.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['apple', 100], 
-    function (err, results, fields) {
-                if (err) throw err;
-        console.log('Inserted ' + results.affectedRows + ' row(s).');
-        })
-    conn.end(function (err) { 
-    if (err) throw err;
-    else  console.log('Done.') 
-    });
-};
+module.exports = db;
